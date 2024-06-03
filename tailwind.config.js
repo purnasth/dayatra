c188227e663247b97ae6a1c19089420664a7258d
Purna Shrestha <crush9918@gmail.com>
2024-06-03 16:39:40 +0545

    fix: responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
         xl: "1280px",
       },
     },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1800px",
+    },
     extend: {
       fontFamily: {
         title: ['"Inria Serif"', "serif"],
